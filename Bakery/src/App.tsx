193b8243c5aa2609487ef375e03b8e0065d074b2
JoZhang06 <joe.chang.1996@hotmail.com>
2023-02-21 16:52:40 -0600

    Add new components to bakery

--- Bakery/src/App.tsx
+++ Bakery/src/App.tsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Products from './components/Products/Products' 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <header>
+      <NavBar/>
+      </header>
+      <section>
       <Products/>
+      </section>
     </div>
   )
 }
