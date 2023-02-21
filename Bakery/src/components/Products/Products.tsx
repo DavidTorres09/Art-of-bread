1f15fa50ea802f3412e5e7565f9d3b6603e07318
DavidTorres09 <angeldtm09@gmail.com>
2023-02-21 17:39:13 -0500

    Fix the CSS in general

--- Bakery/src/components/Products/Products.tsx
+++ Bakery/src/components/Products/Products.tsx
@@ -1,10 +1,18 @@
 import { useState, useRef, useEffect } from "react";
-import Card from "./card";
+import ProductCard from "./card";
+import Bread from '../../images/Bread.jpg';
+
 
 const Products = () => {
   return (
     <div>
-      <Card />
+      <ProductCard
+        name="Pan de maíz"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+
     </div>
   )
 };
