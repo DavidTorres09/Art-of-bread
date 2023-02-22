2aa22396a213d094f41826b2565b2a77a2e163e0
DavidTorres09 <angeldtm09@gmail.com>
2023-02-22 15:44:03 -0500

    Fix the card, and products

--- Bakery/src/components/Products/Products.tsx
+++ Bakery/src/components/Products/Products.tsx
@@ -5,13 +5,122 @@ import Bread from '../../images/Bread.jpg';
 
 const Products = () => {
   return (
-    <div>
+    <div className="container">
       <ProductCard
         name="Pan con queso!!"
         imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
         description="Pos pancito caliente"
         price={2.99}
       />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      <ProductCard
+        name="Pan con queso!!"
+        imageSrc= 'https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        description="Pos pancito caliente"
+        price={2.99}
+      />
+      
 
     </div>
   )
--- Bakery/src/components/Products/card.tsx
+++ Bakery/src/components/Products/card.tsx
@@ -21,6 +21,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
         <h2>{name}</h2>
         <p>{description}</p>
         <p className="price">${price.toFixed(2)}</p>
+        <button>Buy</button>
       </div>
     </div>
   );
