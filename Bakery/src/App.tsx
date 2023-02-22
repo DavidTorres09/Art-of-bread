66c8afb2c0197cf28cf5d2685fa5c409f22acde3
JoZhang06 <joe.chang.1996@hotmail.com>
2023-02-22 15:25:25 -0600

    Add Navbar

--- Bakery/src/App.tsx
+++ Bakery/src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Products from './components/Products/Products' 
+import Navbar from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Navbar/>
       <Products/>
     </div>
   )
---
+++ Bakery/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h3>Icono</h3>
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>More...</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
