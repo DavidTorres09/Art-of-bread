f0462567d89895a1fa4dd873d98ea4b8c18286c1
DavidTorres09 <angeldtm09@gmail.com>
2023-02-22 16:12:25 -0500

    Make the footer

--- Bakery/src/App.tsx
+++ Bakery/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Products from './components/Products/Products' 
+import Footer from './components/Footer/footer' 
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Products/>
+      <Footer/>
     </div>
   )
 }
