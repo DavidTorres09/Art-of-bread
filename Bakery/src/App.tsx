67648033c95617e921a4c58672cd128365dfeaa5
JoZhang06 <joe.chang.1996@hotmail.com>
2023-02-22 15:29:21 -0600

    Skip tsx component navbar

--- Bakery/src/App.tsx
+++ Bakery/src/App.tsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Products from './components/Products/Products' 
-import Navbar from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Navbar/>
+    <div className="App"> 
       <Products/>
     </div>
   )
