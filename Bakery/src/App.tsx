550bd54587028e37064996e86b02823e4ad6ce0c
DavidTorres09 <angeldtm09@gmail.com>
2023-02-23 16:58:26 -0500

    Fix the Css of the cards

--- Bakery/src/App.tsx
+++ Bakery/src/App.tsx
@@ -5,6 +5,7 @@ import Products from './components/Products/Products'
 import Navbar from './components/NavBar/NavBar'
 import Carousel from './components/Carousel/Carousel'
 import Footer from './components/Footer/footer'
+import AboutUs from './components/About us/AboutUs'
 
 
 function App() {
@@ -21,6 +22,9 @@ function App() {
       </section>
       <section id="products">
         <Products />
+      </section>
+      <section id="contact">
+        <AboutUs />
       </section>
       <section id="contact">
         <Footer />
