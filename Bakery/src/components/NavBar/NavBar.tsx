d6ddd3ecf9539811fb8b9c8383dc0761d0d53dd6
JoZhang06 <joe.chang.1996@hotmail.com>
2023-02-23 14:33:16 -0600

    Add some sections

--- Bakery/src/components/NavBar/NavBar.tsx
+++ Bakery/src/components/NavBar/NavBar.tsx
@@ -5,10 +5,10 @@ function Navbar() {
   return (
     <nav>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Products</a></li>
-        <li><a href="#">About us</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#products">Products</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   );
