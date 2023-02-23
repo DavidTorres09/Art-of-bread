b164f56c37540f2ceac705630a728fd083432454
JoZhang06 <joe.chang.1996@hotmail.com>
2023-02-23 16:28:21 -0600

    Add new css

--- Bakery/src/components/NavBar/NavBar.tsx
+++ Bakery/src/components/NavBar/NavBar.tsx
@@ -10,7 +10,6 @@ function Navbar() {
         <li><a href="#products">Products</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
-        <li><a href="#login">Login</a></li>
       </ul>
     </nav>
   );
