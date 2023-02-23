ac8f1cf329b64c541c6ac78ca3cedd9ce1a992a9
JoZhang06 <joe.chang.1996@hotmail.com>
2023-02-23 16:38:37 -0600

    Add new css to background

--- Bakery/src/components/Products/Products.tsx
+++ Bakery/src/components/Products/Products.tsx
@@ -16,50 +16,50 @@ const Products = () => {
           name="Bread with dog face"
           imageSrc='https://images.pexels.com/photos/920220/pexels-photo-920220.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="This dog-shaped bun is perfect for a fun afternoon with friends."
-          price={2.99}
+          price={2.29}
         />
         <ProductCard
           name="Bread with Seeds!"
           imageSrc='https://images.pexels.com/photos/1586947/pexels-photo-1586947.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Delicious seeded bread for a flavorful breakfast."
-          price={2.99}
+          price={1.99}
         />
 
         <ProductCard
           name="Baguette"
           imageSrc='https://images.pexels.com/photos/461060/pexels-photo-461060.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="This baguette is perfect for making a French sandwich."
-          price={2.99}
+          price={2.49}
         />
         <ProductCard
           name="Bread with cheese"
           imageSrc='https://images.pexels.com/photos/13427800/pexels-photo-13427800.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Satisfy your cheese cravings with our freshly baked bread topped with savory melted cheese."
-          price={2.99}
+          price={4.99}
         />
         <ProductCard
           name="Cake"
           imageSrc='https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Indulge in a heavenly treat with our fluffy and decadent cake, perfect for any occasion."
-          price={2.99}
+          price={2.59}
         />
         <ProductCard
           name="Donuts"
           imageSrc='https://images.pexels.com/photos/1191639/pexels-photo-1191639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Indulge yourself with our delicious donuts, available in a variety of flavors and toppings that will make your mouth water."
-          price={2.99}
+          price={2.20}
         />
         <ProductCard
           name="Pancakes"
           imageSrc='https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Whether you like them stacked high or in a neat little stack, these pancakes will make your morning bright."
-          price={2.99}
+          price={1.39}
         />
         <ProductCard
           name="Cupcakes"
           imageSrc='https://images.pexels.com/photos/3081657/pexels-photo-3081657.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Satisfy your cravings with our heavenly cupcakes, bursting with flavor and finished with a delicate swirl of frosting."
-          price={2.99}
+          price={4.00}
         />
         <ProductCard
           name="Macarons"
@@ -71,7 +71,7 @@ const Products = () => {
           name="Cookies"
           imageSrc='https://images.pexels.com/photos/298217/pexels-photo-298217.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Sweet and crunchy, our freshly-baked cookies are the perfect treat for any occasion."
-          price={2.99}
+          price={3.50}
         />
         <ProductCard
           name="Coffee"
@@ -83,7 +83,7 @@ const Products = () => {
           name="Frappe"
           imageSrc='https://images.pexels.com/photos/214333/pexels-photo-214333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
           description="Frappe, the perfect drink to cool off and enjoy a delicious blend of coffee and cream."
-          price={2.99}
+          price={1.99}
         />
         
         
